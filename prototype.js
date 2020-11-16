c873c34c7f7a09f75d106c896a9e5466e8f2b6b7
jy <jaey5oung@naver.com>
2020-11-16 23:34:14 +0900

    es6 first

--- prototype.js
+++ prototype.js
@@ -1,5 +1,50 @@
-var superObj = { superVal: "super" }
-var subObj = { subVal: "sub" }
-subObj.__proto__ = superObj
-console.log("subObj.subVal=>", subObj.subVal)
-console.log("subObj.superVal=>", subObj.superVal)
+// var superObj = { superVal: "super" }
+// var subObj = { subVal: "sub" }
+// subObj.__proto__ = superObj
+// console.log("subObj.subVal=>", subObj.subVal)
+// console.log("subObj.superVal=>", subObj.superVal)
+
+// var superObj = { superVal: "super" }
+// var subObj = Object.create(superObj)
+// subObj.subVal = "sub"
+
+// var kim = {
+//   name: "kim",
+//   first: 10,
+//   second: 20,
+//   sum: function () {
+//     return this.first + this.second
+//   }
+// var jang = Object.create(kim);
+//  {
+//       name='lee',
+//       first:10, second:10,
+//       avg:function(){
+//           return(this.first+this.second)/2;
+//       }
+//   }
+//   lee.__proto__=kim;
+//   console.log(lee.sum());
+//   console.log(lee.avg());
+
+// }
+
+// console.log(kim.sum())
+
+ var name = "global";
+
+function a(){
+   var home = "homeover";
+  for(let i=0; i<100; i++){
+    console.log(i)
+  } 
+   console.log(i)
+}
+a();
+
+var list = document.querySelectorAll("li");
+for(var i=0; i<list.length; i++){
+  list[i].addEventListener("click",function(){
+    console.log(i + "번째 리스트입니다");
+  });
+}
