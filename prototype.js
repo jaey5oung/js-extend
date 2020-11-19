0d756d6a14c87a6db54229905c5d7fd20c9daa32
jy <jaey5oung@naver.com>
2020-11-19 16:47:14 +0900

    const,immutable array

--- prototype.js
+++ prototype.js
@@ -31,20 +31,45 @@
 
 // console.log(kim.sum())
 
- var name = "global";
+//  var name = "global";
 
-function a(){
-   var home = "homeover";
-  for(let i=0; i<100; i++){
-    console.log(i)
-  } 
-   console.log(i)
-}
-a();
+// function a(){
+//    var home = "homeover";
+//   for(let i=0; i<100; i++){
+//     console.log(i)
+//   } 
+//    console.log(i)
+// }
+// a();
 
-var list = document.querySelectorAll("li");
-for(var i=0; i<list.length; i++){
-  list[i].addEventListener("click",function(){
-    console.log(i + "번째 리스트입니다");
-  });
-}
+// var list = document.querySelectorAll("li");
+// for(var i=0; i<list.length; i++){
+//   list[i].addEventListener("click",function(){
+//     console.log(i + "번째 리스트입니다");
+//   });
+// }
+
+
+// function home() {
+//   const homename = [1,2,3,3];
+//    homename =["1"]
+// //   homename = "your house";
+//   console.log(homename);
+  
+// }
+
+//home();
+//const를 기본으로 사용한다
+//그런데 변경이 될 수 있는 변수는 let을 사용한다.
+
+function home() {
+    const list = ["apple", "orange", "watermelon"];
+    list.push("banana");
+    console.log(list);
+  }
+  //immutable array를 어떻게 만들지?
+  const list = ["apple", "orange", "watermelon"];
+  list2 = [].concat(list, "banana");
+  console.log(list === list2);
+  
+  //const 는 재할당은안되지만 오브젝트나 배열을 추가하거나 삭제할수있다
